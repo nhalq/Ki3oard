5bd056c7fbeb32a3a19ac3582517ba155816e108
nhalq <nhalq287@gmail.com>
2021-07-01 23:00:12 +0700

    Ki3oard

---
+++ Ki3oard/Ki3oard.cpp
@@ -0,0 +1,15 @@
+// Ki3oard.cpp 
+// Code: Nha Le
+
+#include <Windows.h>
+
+int APIENTRY wWinMain(_In_     HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_     LPWSTR    lpCmdLine,
+                      _In_     int       nCmdShow)
+{
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
+  
+  return EXIT_SUCCESS; 
+}
